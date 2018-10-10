c976844831056d2da61c5dfd43df43af5e1b3c15
ALFON <afernandezrio0@uoc.edu>
2018-10-10 18:09:25 +0200

    Snakis

---
+++ Snakis/Snakis/js/js.js
@@ -0,0 +1,20 @@
+
+		function mostrarlin() {
+			document.getElementById("mostrarlineal").style.display = "block";
+			document.getElementById("mostrargrid").style.display = "none";
+		}
+
+		function mostrargr() {
+			document.getElementById("mostrarlineal").style.display = "none";
+			document.getElementById("mostrargrid").style.display = "block";
+		}
+
+		function mostrarlin2() {
+			document.getElementById("mostrarlineal2").style.display = "block";
+			document.getElementById("mostrargrid2").style.display = "none";
+		}
+
+		function mostrargr2() {
+			document.getElementById("mostrarlineal2").style.display = "none";
+			document.getElementById("mostrargrid2").style.display = "inherit";
+		}
